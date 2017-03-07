dab4c4989c3e06ba4f3c3a69486f01b3a668eb8d
jtyler <jtyler.dev@gmail.com>
2017-03-07 02:01:53 -0800

    adding in basic react framework

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,27 @@ var BUILD_DIR = path.resolve(__dirname, 'dist');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-    entry: APP_DIR + '/index.js',
+    entry: APP_DIR + '/app/main.js',
     output: {
         path: BUILD_DIR,
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: "/app/"
+    },
+    devServer: {
+        inline: true,
+        port: 3000
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
     }
 };
 
